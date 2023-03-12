0dcb8a6ad88790c0ae0326cb81130ea554212330
jimmythewebdeveloper <125082096+jimmythewebdeveloper@users.noreply.github.com>
2023-03-12 17:11:44 -0400

    lesson 21.0 assertions

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,10 +36,14 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({
+          surname: "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type);
+          assert.equal(res.body.name);
+          assert.equal(res.body.surname);
           done();
         });
     });
