7018d1d52e4ad1d4cf88c5df182d323145563e2e
jimmythewebdeveloper <125082096+jimmythewebdeveloper@users.noreply.github.com>
2023-03-12 17:55:34 -0400

    lesson 21.2 assertions

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -42,7 +42,7 @@ suite('Functional Tests', function () {
         .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(res.type, 'application/json');
-          assert.equal(res.body.name, 'Christoforo');
+          assert.equal(res.body.name, 'Cristoforo');
           assert.equal(res.body.surname, 'Colombo');
           done();
         });
