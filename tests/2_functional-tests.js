dfd4d9ba049b7f10c8edf391c7a7f1adaa13a00d
jimmythewebdeveloper <125082096+jimmythewebdeveloper@users.noreply.github.com>
2023-03-12 23:07:04 -0400

    lesson 23.2 assertions

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -66,11 +66,11 @@ suite('Functional Tests', function () {
 
 const Browser = require('zombie');
 Browser.site = 'https://chai-szbj.onrender.com';
-const browser = new Broswer();
 
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
   suiteSetup((done) => {
     return browser.visit('/', done);
   });
